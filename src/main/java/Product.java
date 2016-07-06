66cb6a66581df1c8549b64eb39fca35869b0509b
linesh <linesh.simpcity@gmail.com>
2016-07-06 18:55:20 +0800

    Congyu: Add test case: shouldn't be offered discount when product not on list

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -1,2 +1,11 @@
 public class Product {
+    private String name;
+
+    public Product(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/test/java/PromotionCalculatorTest.java
+++ src/test/java/PromotionCalculatorTest.java
@@ -15,7 +15,7 @@ public class PromotionCalculatorTest {
 
     @Test
     public void should_not_be_received_a_discount_when_asking_for_promotion_given_products_is_on_discount_list_but_quantity_is_less_than_3() throws Exception {
-        Product apple = new Product();
+        Product apple = new Product("apple");
 
         boolean shouldBeOfferedDiscount = promotionCalculator.checkIfProductCanBeOfferedADiscount(apple, 1);
 
@@ -24,10 +24,19 @@ public class PromotionCalculatorTest {
 
     @Test
     public void should_be_received_a_discount_when_asking_for_promotion_given_products_is_on_discount_list_and_quantity_is_more_than_3() throws Exception {
-        Product apple = new Product();
+        Product apple = new Product("apple");
 
         boolean shouldBeOfferedDiscount = promotionCalculator.checkIfProductCanBeOfferedADiscount(apple, 3);
 
         assertThat(shouldBeOfferedDiscount, is(true));
+    }
+
+    @Test
+    public void should_not_be_received_a_discount_when_asking_for_promotion_given_product_is_not_on_discount_list() throws Exception {
+        Product cokacola = new Product("cokacola");
+
+        boolean shouldBeOfferedDiscount = promotionCalculator.checkIfProductCanBeOfferedADiscount(cokacola, 3);
+
+        assertThat(shouldBeOfferedDiscount, is(false));
     }
 }
