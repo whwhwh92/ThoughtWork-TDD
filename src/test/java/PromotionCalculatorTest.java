5cffe064a150386d19747df8f1364f39500d5bde
linesh <linesh.simpcity@gmail.com>
2016-07-06 18:48:23 +0800

    Congyu: Add test case: should be offered a discount when quantity is 3

--- src/test/java/PromotionCalculatorTest.java
+++ src/test/java/PromotionCalculatorTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
@@ -5,13 +6,28 @@ import static org.junit.Assert.assertThat;
 
 public class PromotionCalculatorTest {
 
+    private PromotionCalculator promotionCalculator;
+
+    @Before
+    public void setUp() throws Exception {
+        promotionCalculator = new PromotionCalculator();
+    }
+
     @Test
-    public void should_not_be_received_a_discount_when_asking_for_promotion_given_products_is_not_in_discount_list() throws Exception {
-        PromotionCalculator promotionCalculator = new PromotionCalculator();
+    public void should_not_be_received_a_discount_when_asking_for_promotion_given_products_is_on_discount_list_but_quantity_is_less_than_3() throws Exception {
         Product apple = new Product();
 
-        boolean offeredDiscount = promotionCalculator.checkIfProductIsInDiscountList(apple);
+        boolean shouldBeOfferedDiscount = promotionCalculator.checkIfProductCanBeOfferedADiscount(apple, 1);
 
-        assertThat(offeredDiscount, is(false));
+        assertThat(shouldBeOfferedDiscount, is(false));
+    }
+
+    @Test
+    public void should_be_received_a_discount_when_asking_for_promotion_given_products_is_on_discount_list_and_quantity_is_more_than_3() throws Exception {
+        Product apple = new Product();
+
+        boolean shouldBeOfferedDiscount = promotionCalculator.checkIfProductCanBeOfferedADiscount(apple, 3);
+
+        assertThat(shouldBeOfferedDiscount, is(true));
     }
 }
