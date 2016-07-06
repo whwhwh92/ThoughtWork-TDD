c4180b270baac35731d06c4ea06f928942cd9f09
linesh <linesh.simpcity@gmail.com>
2016-07-06 21:57:58 +0800

    Congyu: Refactor test, try to make tests more readable

--- src/test/java/OnPromotionProductsListTest.java
+++ src/test/java/OnPromotionProductsListTest.java
@@ -11,33 +11,45 @@ import static org.mockito.MockitoAnnotations.initMocks;
 public class OnPromotionProductsListTest {
 
     private OnPromotionProductsList onPromotionProductsList;
-    private List<PromotionProduct> promotionProducts;
+    private List<PromotionProduct> promotionProducts = new ArrayList<>();
 
     @Before
     public void setUp() throws Exception {
         initMocks(this);
-        promotionProducts = new ArrayList<>();
-        PromotionProduct apple = new PromotionProduct();
-        apple.setProductName("apple");
-        promotionProducts.add(apple);
-        onPromotionProductsList = new OnPromotionProductsList(promotionProducts);
     }
-
     @Test
     public void should_return_true_when_product_is_apple_given_it_is_on_promotion_list() throws Exception {
         Product apple = new Product("apple");
+        Product cokacola = new Product("cokacola");
 
-        boolean onPromotionList = onPromotionProductsList.contains(apple);
+        addProductsToPromotionList("apple", "cokacola");
+        onPromotionProductsList = new OnPromotionProductsList(promotionProducts);
 
-        assertThat(onPromotionList, is(true));
+        boolean appleOnPromotion = onPromotionProductsList.contains(apple);
+        boolean cokacolaOnPromotion = onPromotionProductsList.contains(cokacola);
+
+        assertThat(appleOnPromotion, is(true));
+        assertThat(cokacolaOnPromotion, is(true));
     }
 
     @Test
     public void should_return_false_when_product_is_cokacola_given_it_is_not_on_promotion_list() throws Exception {
         Product hammer = new Product("hammer");
 
+        addProductsToPromotionList("apple");
+        onPromotionProductsList = new OnPromotionProductsList(promotionProducts);
+
         boolean onPromotionList = onPromotionProductsList.contains(hammer);
 
         assertThat(onPromotionList, is(false));
     }
+
+    private void addProductsToPromotionList(String... productNames) {
+        for (String productName : productNames) {
+            PromotionProduct product = new PromotionProduct();
+            product.setProductName(productName);
+            promotionProducts.add(product);
+        }
+    }
+
 }
