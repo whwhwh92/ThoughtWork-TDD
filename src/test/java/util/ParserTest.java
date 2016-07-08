470d7aee2db8de2ca8aca835ea804df4495c6632
Sora Lee <sora_lsh@163.com>
2016-07-08 16:49:54 +0800

    Xiaohao: Refactor goods-parsing test and add offer-parsing test

--- src/test/java/util/ParserTest.java
+++ src/test/java/util/ParserTest.java
@@ -14,6 +14,7 @@ import static org.junit.Assert.*;
 public class ParserTest {
 
     private static final String JSON_GOODS = "goods.json";
+    private static final String JSON_OFFER = "offer.json";
 
     private static InputStream getStream(String res) {
         return Parser.class.getClassLoader()
@@ -23,8 +24,9 @@ public class ParserTest {
     @Test
     public void should_parse_right_goods_info_from_json_file()
             throws Exception {
-        HashMap<String, Goods> goodsMap = Parser.readFromJsonStream(
-                ParserTest.getStream(JSON_GOODS));
+        HashMap<String, Goods> goodsMap = Parser
+                .readGoodsFromJsonStream(
+                        ParserTest.getStream(JSON_GOODS));
         assertEquals(6, goodsMap.size());
 
         Goods goods = goodsMap.get("ITEM000003");
@@ -38,4 +40,34 @@ public class ParserTest {
         assertEquals(5.50f, goods.getPrice(), 0.001f);
     }
 
+    private static Goods genOfferGoods() {
+        Goods goods = new Goods();
+        goods.setBarcode("ITEM000001");
+        goods.setName("可口可乐");
+        goods.setCategory("食品");
+        goods.setSubCategory("饮料");
+        goods.setUnit("瓶");
+        goods.setPrice(3.00f);
+        return goods;
+    }
+
+    private static Goods genNonOfferGoods() {
+        Goods goods = new Goods();
+        goods.setBarcode("ITEM000003");
+        goods.setName("苹果");
+        goods.setCategory("食品");
+        goods.setSubCategory("水果");
+        goods.setUnit("斤");
+        goods.setPrice(5.50f);
+    }
+
+    @Test
+    public void should_parse_right_offer_info_from_json_file()
+            throws Exception {
+        Offer offer = Parser.readOfferFromJsonStream(
+                ParserTest.getStream(JSON_OFFER));
+
+        assertTrue(offer.check(ParserTest.genOfferGoods()));
+        assertFalse(offer.check(ParserTest.genNonOfferGoods()));
+    }
 }
